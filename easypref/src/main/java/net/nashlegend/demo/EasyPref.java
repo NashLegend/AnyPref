9e1d0372c2862f2107da2c349f5b63268a47c842
NashLegend <panzhihuipzh@gmail.com>
2016-05-23 17:41:15 +0800

    speedup

--- easypref/src/main/java/net/nashlegend/demo/EasyPref.java
+++ easypref/src/main/java/net/nashlegend/demo/EasyPref.java
@@ -8,20 +8,34 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by NashLegend on 16/5/20.
  * 应该满足动态定义PrefName的需求，比如为不同的用户添加不同的名字
  */
+@SuppressWarnings("unchecked")
 public class EasyPref {
+
+    private static ConcurrentHashMap<String, Field[]> fieldsMap = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Boolean> setMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
 
     }
 
+    private static Field[] getFields(Class clazz) {
+        Field[] fields = fieldsMap.get(clazz.getCanonicalName());
+        if (fields == null) {
+            fields = clazz.getFields();
+            fieldsMap.put(clazz.getCanonicalName(), fields);
+        }
+        return fields;
+    }
+
     public static <T> T get(Class<T> clazz, Context context) throws IllegalAccessException, InstantiationException {
         T obj = clazz.newInstance();
-        Field[] fields = clazz.getFields();
+        Field[] fields = getFields(clazz);
         for (Field field : fields) {
             int modifier = field.getModifiers();
             if (Modifier.isFinal(modifier) || Modifier.isStatic(modifier)) {
@@ -35,7 +49,7 @@ public class EasyPref {
     public static void apply(Object object, Context context) {
         SharedPreferences.Editor editor = context.getSharedPreferences(object.getClass().getCanonicalName(),
                 Context.MODE_PRIVATE).edit();
-        for (Field field : object.getClass().getFields()) {
+        for (Field field : getFields(object.getClass())) {
             int modifier = field.getModifiers();
             if (Modifier.isFinal(modifier) || Modifier.isStatic(modifier)) {
                 continue;
@@ -66,21 +80,29 @@ public class EasyPref {
                     editor.putString(key, String.valueOf(field.get(object)));
                     break;
                 default:
-                    if (field.getType().isAssignableFrom(Set.class)) {
-                        Type tp = field.getGenericType();
-                        if (tp == null)
-                            break;
-                        if (tp instanceof ParameterizedType) {
-                            ParameterizedType pt = (ParameterizedType) tp;
-                            Type[] types = pt.getActualTypeArguments();
-                            if (types.length == 1) {
-                                Class genericClazz = (Class) pt.getActualTypeArguments()[0];
-                                if (genericClazz.getCanonicalName().equals(String.class.getCanonicalName())) {
-                                    Set<String> stringSet = (Set<String>) field.get(object);
-                                    editor.putStringSet(key, stringSet);
+                    String mapKey = object.getClass().getCanonicalName() + "$$" + key;
+                    Boolean result = setMap.get(mapKey);
+                    if (result == null) {
+                        if (field.getType().isAssignableFrom(Set.class)) {
+                            Type tp = field.getGenericType();
+                            if (tp != null && tp instanceof ParameterizedType) {
+                                ParameterizedType pt = (ParameterizedType) tp;
+                                Type[] types = pt.getActualTypeArguments();
+                                if (types.length == 1) {
+                                    Class genericClazz = (Class) pt.getActualTypeArguments()[0];
+                                    if (genericClazz.getCanonicalName().equals(String.class.getCanonicalName())) {
+                                        setMap.put(mapKey, true);
+                                        Set<String> stringSet = (Set<String>) field.get(object);
+                                        editor.putStringSet(key, stringSet);
+                                        break;
+                                    }
                                 }
                             }
                         }
+                        setMap.put(mapKey, false);
+                    } else if (result) {
+                        Set<String> stringSet = (Set<String>) field.get(object);
+                        editor.putStringSet(key, stringSet);
                     }
                     break;
             }
@@ -110,20 +132,27 @@ public class EasyPref {
                     field.set(object, preferences.getString(key, null));
                     break;
                 default:
-                    if (field.getType().isAssignableFrom(Set.class)) {
-                        Type tp = field.getGenericType();
-                        if (tp == null)
-                            break;
-                        if (tp instanceof ParameterizedType) {
-                            ParameterizedType pt = (ParameterizedType) tp;
-                            Type[] types = pt.getActualTypeArguments();
-                            if (types.length == 1) {
-                                Class genericClazz = (Class) pt.getActualTypeArguments()[0];
-                                if (genericClazz.getCanonicalName().equals(String.class.getCanonicalName())) {
-                                    field.set(object, preferences.getStringSet(key, null));
+                    String mapKey = object.getClass().getCanonicalName() + "$$" + key;
+                    Boolean result = setMap.get(mapKey);
+                    if (result == null) {
+                        if (field.getType().isAssignableFrom(Set.class)) {
+                            Type tp = field.getGenericType();
+                            if (tp != null && tp instanceof ParameterizedType) {
+                                ParameterizedType pt = (ParameterizedType) tp;
+                                Type[] types = pt.getActualTypeArguments();
+                                if (types.length == 1) {
+                                    Class genericClazz = (Class) pt.getActualTypeArguments()[0];
+                                    if (genericClazz.getCanonicalName().equals(String.class.getCanonicalName())) {
+                                        setMap.put(mapKey, true);
+                                        field.set(object, preferences.getStringSet(key, null));
+                                        break;
+                                    }
                                 }
                             }
                         }
+                        setMap.put(mapKey, false);
+                    } else if (result) {
+                        field.set(object, preferences.getStringSet(key, null));
                     }
                     break;
             }
