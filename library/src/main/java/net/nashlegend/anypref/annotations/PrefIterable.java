3dbbfaa8973412bfed81b597d69a02bf87713a5b
NashLegend <panzhihuipzh@gmail.com>
2016-06-08 15:38:20 +0800

    adding Iterable and Array TODO

---
+++ library/src/main/java/net/nashlegend/anypref/annotations/PrefIterable.java
@@ -0,0 +1,16 @@
+package net.nashlegend.anypref.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by NashLegend on 16/6/3.
+ */
+@Target(FIELD)
+@Retention(RUNTIME)
+public @interface PrefIterable {
+    // TODO: 16/6/8  
+}
